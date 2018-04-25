c87136c10de94bc2cbc489c3b34c145e03f8b6ae
Aneesh Bhadran <aneeshbhadran@gmail.com>
2018-04-25 06:50:22 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+
+//services
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const classifier = require('./nlu/classifier.js');
+
+
+// models
+const Reply = require('./reply.js');
+
+
+// socket.io channels 
+const messageChannel = 'message';
+const replyChannel = 'reply';
+
+app.use('/', express.static(path.join(__dirname + '/public')));
+
+io.on('connection', function (socket) {
+  console.log("User connected to Chatbot");
+  socket.emit(replyChannel, new Reply("init", JSON.parse('{"name": "init1"}'), "").toJson());
+  socket.emit(replyChannel, new Reply("init", JSON.parse('{"name": "init2"}'), "").toJson());
+  socket.on(messageChannel, function (message, isUser, fn) {
+      
+      fn('Message arrived to the server'); //callback function
+      sendToBot(message, socket);
+      
+  });
+
+  socket.on(replyChannel, function(message, intent, feedback){
+    console.log("Message: " + message +" | Intent: " +intent +" | Feedback: " + feedback);
+  });
+});
+
+var port = 8000;
+server.listen(port, function () {
+  console.log('Chatbot is listening on port ' + port + '!')
+});
+
+sendToBot = function(message, socket){  
+  classifier.parse(message, function (error, intent, entities) {
+    if (error) {
+      socket.emit(replyChannel, "An error has occurred: " + error);
+    } else {
+      socket.emit(replyChannel, new Reply(message, intent, entities).toJson());
+    }
+  });
+}
